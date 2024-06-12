526f41e6b2d26a5a92f858fcd2491bda676ca49d
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 03:14:05 +0000

    oo1

--- test2.java
+++ test2.java
@@ -2,13 +2,9 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args){
 
-    System.out.println("Welcome To BackendLand");
-  
-    System.out.println("El dia 1 de Diciembre ya tengo que esta trabajando en Java con el Framework Spring");
-      System.out.println("Que bonito es lo bonito");
-  }
-}
-    
-  }
-  
+System.out.println("Welcome To BackendLand");
+System.out.println("El dia 1 de Diciembre ya tengo que esta trabajando en Java con el Framework Spring");
+System.out.println("Que bonito es lo bonito");
+
+  }  
 } 
