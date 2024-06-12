43bfc7dcedb0c696154074ddebc095eb0d7a28bb
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 03:03:42 +0000

    test3

--- test.java
+++ test.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args){
 
-    System.out.println("Welcome To BackendLand")
+    System.out.println("Welcome To BackendLand");
     Scanner entrada = new Scanner(System.in);
     int numero;
     System.out.println("Ingressaaa muchos numeroissadasdase mero:");
     numero = entrada.nextInt();
     System.out.println("El numero ingresado es: "+numero);
+    System.out.println("El dia 1 de Diciembre ya tengo que esta trabajando en Java con el Framework Spring");
       
   }
 }
