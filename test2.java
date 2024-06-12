d65b8b4eb189787689f2602e5ee6cecaee4da5bc
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 02:06:48 +0000

    test2.0

--- test2.java
+++ test2.java
@@ -1,1 +1,6 @@
-System.out.println("Este es mi Hola mundo")
+import java.util.Scanner;
+class test2{
+  public static void main(String[] args){
+System.out.println("Este es mi Hola mundo");
+  }
+} 
