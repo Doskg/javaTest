40d5581d9728ea6f51e230d450bf70a0494dde19
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 02:35:06 +0000

    commit 2

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ class Main {
     System.out.println("Welcome To BackendLand")
     Scanner entrada = new Scanner(System.in);
     int numero;
-    System.out.println("Ingresa un numero:");
+    System.out.println("Ingressaaa muchos numeroissadasdase mero:");
     numero = entrada.nextInt();
     System.out.println("El numero ingresado es: "+numero);
       
