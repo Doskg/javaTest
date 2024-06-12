1e3d8b34e735ce048ad940b2bbbdcc75aa07b8dd
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 02:07:55 +0000

    scannumber

--- test2.java
+++ test2.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 class test2{
   public static void main(String[] args){
-System.out.println("Este es mi Hola mundo");
+  System.out.println("Este es mi Hola mundo");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int number = scanner.nextInt();
+  
   }
+  
 } 
