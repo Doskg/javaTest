56606fb2247bad5e893f57d7c2347fd9b28fcd2b
Oscar González Baldizon <55630646+Doskg@users.noreply.github.com>
2024-06-12 01:51:26 +0000

    Initial commit

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+    int numero;
+    System.out.println("Ingresa un numero:");
+    numero = entrada.nextInt();
+    System.out.println("El numero ingresado es: "+numero);
+      
+  }
+}
